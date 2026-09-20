6068097be81e0274d01176a704a3d5c3ef733df9
Pol Hernandez <kaulfield@protonmail.com>
2026-09-20 21:01:51 +0200

    check-update in vscode wasnt needed

--- fedora_toolkit.sh
+++ fedora_toolkit.sh
@@ -41,7 +41,6 @@ install_virt_manager() {
 install_vscode() {
     rpm --import https://packages.microsoft.com/keys/microsoft.asc
     echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo
-    dnf check-update
     dnf install -y code
 }
 
